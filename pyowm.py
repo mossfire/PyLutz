f14944a8a7f95a14f06a29aa936dc1113723f560
Konstantin Sukhanov <mossfire@mail.ru>
2022-05-14 19:07:24 +0700

    qt

---
+++ pyowm.py
@@ -0,0 +1,13 @@
+#работа с OWM сервисом погоды
+import pyowm
+import PyQt5
+from PyQt5 import QtWidgets
+import sys
+app=QtWidgets.QApplication(sys.argv)
+window = QtWidget.QWidget()
+window.setWindowsTitle('Liilith SaqAta')
+
+from pyowm.owm import OWM
+owm=OWM('0468445dd48b09d6e3b247de97195500')
+weather_mgr = owm.weather_manager()
+observation = mgr.weather_at_place('Moscow,RU')
--- test.py
+++ test.py
@@ -1,4 +1,7 @@
+from functools import reduce
+
+
 F=open('script.py')
 
-for line in F:
-    print(line.upper(), end='')
+L=[line.rstrip().upper() for line in open('script.py')  if line[0] !='p' ]
+print(L)
