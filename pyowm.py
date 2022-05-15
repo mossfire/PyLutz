922671b3a66654ca0cad72fefd4899458d8ccee5
Konstantin Sukhanov <mossfire@mail.ru>
2022-05-15 13:09:26 +0700

    qt

--- pyowm.py
+++ pyowm.py
@@ -1,4 +1,14 @@
-#работа с OWM сервисом погоды
+'''
+#Работа с сервисом погоды OWM
+from pyowm.owm import OWM
+owm=OWM('FreeApiKey') # TODO: hide FreeApiKey: 0468445dd48b09d6e3b247de97195500
+weather_mgr = owm.weather_manager()
+observation = mgr.weather_at_place('Moscow,RU')
+'''
+
+
+#работа с QT
+from PyQt5.QtCore import QWaitCondition
 import pyowm
 import PyQt5
 from PyQt5 import QtWidgets
@@ -6,8 +16,18 @@ import sys
 app=QtWidgets.QApplication(sys.argv)
 window = QtWidgets.QWidget()
 window.setWindowTitle('Liilith SaqAta')
+window.resize(250,100)
+label=QtWidgets.QLabel('<center> <b>It is a good day to Die</b></center>')
+btnQuit=QtWidgets.QPushButton('&Close')
+btnBeep=QtWidgets.QPushButton('&Beep')
+vbox=QtWidgets.QVBoxLayout()
+vbox.addWidget(label)
+vbox.addWidget(btnQuit)
+vbox.addWidget(btnBeep)
+window.setLayout(vbox)
+btnQuit.clicked.connect(app.quit)
+btnBeep.clicked.connect(app.beep)
 
-from pyowm.owm import OWM
-owm=OWM('FreeApiKey') # TODO: hide FreeApiKey: 0468445dd48b09d6e3b247de97195500
-weather_mgr = owm.weather_manager()
-observation = mgr.weather_at_place('Moscow,RU')
+window.show()
+sys.exit(app.exec_())
+
