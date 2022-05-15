8908afb5367ba9e580e840d51ea881434aec06da
Konstantin Sukhanov <mossfire@mail.ru>
2022-05-15 11:52:13 +0700

    er

--- pyowm.py
+++ pyowm.py
@@ -1,13 +1,13 @@
 #работа с OWM сервисом погоды
-#import pyowm
+import pyowm
 import PyQt5
 from PyQt5 import QtWidgets
 import sys
 app=QtWidgets.QApplication(sys.argv)
-window = QtWidget.QWidget()
-window.setWindowsTitle('Liilith SaqAta')
+window = QtWidgets.QWidget()
+window.setWindowTitle('Liilith SaqAta')
 
 from pyowm.owm import OWM
-owm=OWM('0468445dd48b09d6e3b247de97195500')
+owm=OWM('FreeApiKey') # TODO: hide FreeApiKey: 0468445dd48b09d6e3b247de97195500
 weather_mgr = owm.weather_manager()
 observation = mgr.weather_at_place('Moscow,RU')
